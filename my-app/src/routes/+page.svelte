43c4fed49aa23d4b2ca4f5e5f988711906db46cc
Felipe-dSantos <lipehh18@gmail.com>
2023-07-20 15:23:30 -0300

    Project-svelte

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,53 @@
+<div  class="container" >
+    <h1>MEU SITE</h1>
+
+    <ul>
+        <li><a href="/contador">Contador</a></li>
+        <li><a href="/sobre">sobre</a></li>
+    </ul>
+</div>
+
+<style>
+    div.container {
+        display: grid;
+        text-align: center;
+        background-color: #ccc;
+        justify-content: center;
+        width: 30%;
+        border-radius: 18px;
+        padding: 15px;
+        margin: auto;
+    }
+    ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    ul li {
+        margin: auto;
+        margin: 5px;
+    }
+
+    a {
+        width: 200px;
+        display: block;
+        background-color: blue;
+        border-radius: 10px;
+        padding: 10px;
+        text-decoration: none;
+        color: white;
+        cursor: pointer;
+    }
+
+    a:hover {
+        background-color: rgb(2, 2, 143);
+    }
+    h1,
+    a {
+        text-align: center;
+        font-family: "Nunito", sans-serif;
+    }
+</style>
---
+++ my-app/src/routes/contador/+page.svelte
@@ -0,0 +1,68 @@
+<script>
+    import NovoComponent from "../NovoComponent.svelte";
+
+    let count = 0;
+    $: doubled = count * 2;
+
+    $: if (count >= 1) {
+		alert('Esse Titulo não Existe, 51 é pinga!');
+		count = 0;
+	}
+
+    function increment() {
+        count += 1;
+    }
+    function decrement() {
+        count -= 1;
+    }
+
+    function zerar() {
+        count = 0;
+    }
+</script>
+
+<div class="container">
+    <h1>Contador</h1>
+    <button on:click={increment}>+</button>
+
+    <span> {count}</span>
+
+    <button on:click={decrement}>-</button>
+
+    <button on:click={zerar}> zerar </button>
+
+    <p>{count} O dobro é {doubled}</p>
+</div>
+
+<style>
+    div.container {
+        text-align: center;
+        background-color: #2c2c2c;
+        color: white;
+        width: 30%;
+        border-radius: 18px;
+        padding: 15px;
+        margin: auto;
+        font-family: "Nunito", sans-serif;
+    }
+
+    button{
+        color: white;
+        border-radius: 10px;
+        padding: 10px;
+        text-decoration: none;
+        border: none;
+        background: none;
+        cursor: pointer;
+        font-size: 50px;
+    }
+
+    button:hover {
+        color: #FF3E00;
+    }
+
+    span{
+        text-align: center;
+        font-size: 40px;
+    }
+</style>
